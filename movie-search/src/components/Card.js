78ef5f938e7427655057ff7e0a3569f907bcfca6
Nav Gill <navgill@Navs-MacBook-Air.local>
2022-02-07 18:17:27 -0700

    react app created

---
+++ movie-search/src/components/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Card() {
+  return (
+    <div className="resultsCard">
+      <figure className="image is-48x48">
+        <img src="" alt="" />
+      </figure>
+      <h4>Movie Title</h4>
+      <span>Year:2020</span>
+    </div>
+  );
+}
---
+++ movie-search/src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SearchBar() {
+  return (
+    <div>
+      <div className="control">
+        <input className="input" type="text" placeholder="Search Movie"></input>
+      </div>
+    </div>
+  );
+}
+
+export default SearchBar;
